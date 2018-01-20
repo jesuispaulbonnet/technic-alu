a2dd784381a2279187594bdabbe95409841cd571
Paul Bonnet <Paul Bonnet>
2018-01-20 17:51:06 +0000

    Add the carousel in the home page

--- home/models.py
+++ home/models.py
@@ -2,12 +2,36 @@ from __future__ import absolute_import, unicode_literals
 
 from django.db import models
 
-from wagtail.wagtailcore.models import Page
+from modelcluster.fields import ParentalKey
+
+from wagtail.wagtailcore.models import Page, Orderable
+from wagtail.wagtailadmin.edit_handlers import InlinePanel
+from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 
 
 class HomePage(Page):
-    pass
+
+    content_panels = Page.content_panels + [
+        InlinePanel('carousel_images', label="Carousel Images"),
+    ]
 
 
 class GalleryPage(Page):
     pass
+
+class CarouselImage(Orderable):
+    page = ParentalKey(
+        HomePage,
+        on_delete=models.CASCADE,
+        related_name='carousel_images'
+    )
+
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        on_delete=models.CASCADE,
+        related_name='+'
+    )
+
+    panels = [
+        ImageChooserPanel('image'),
+    ]
--- technic_alu/static/js/technic_alu.js
+++ technic_alu/static/js/technic_alu.js
@@ -1,5 +1,10 @@
 (function($) {
   $(function() {
       $('.button-collapse').sideNav();
+      $('.carousel.carousel-slider').carousel(
+          {
+            duration: 100
+          }
+      );
   }); // end of document ready
 })(jQuery); // end of jQuery name space
