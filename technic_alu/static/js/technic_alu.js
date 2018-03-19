6aeebf6b790a69a64925b1f2e714ba74fb389fc6
Paul Bonnet <jesuispaulbonnet@gmail.com>
2018-03-19 21:44:37 +0000

    Fix menu

--- technic_alu/static/js/technic_alu.js
+++ technic_alu/static/js/technic_alu.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
     $('.button-collapse').sideNav();
-    $('.dropdown-button').dropdown();
+    $('.dropdown-button').dropdown({
+        'constrainWidth': false
+    });
     $('.collapsible').collapsible();
 });
